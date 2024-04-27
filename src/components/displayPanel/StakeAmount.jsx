0af426886b5e9cc895be3f2b4e4440c12a81333a
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-04-27 13:53:13 +0100

    Update

--- src/components/displayPanel/StakeAmount.jsx
+++ src/components/displayPanel/StakeAmount.jsx
@@ -5,7 +5,7 @@ const StakeAmount = () => {
     <div className='' > 
         <h1 className='font-bold text-4xl'>Stake <br /> Amount:</h1>
         <p className="text-center mt-3 text-2xl">0</p>
-    </div> 
+    </div>  
   )
 }
 
