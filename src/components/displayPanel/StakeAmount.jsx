6a6c5ab234195b33e3b45494b56544c104f594ef
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-04-27 13:52:13 +0100

    update

--- src/components/displayPanel/StakeAmount.jsx
+++ src/components/displayPanel/StakeAmount.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const StakeAmount = () => {
   return (
-    <div className='' >
+    <div className='' > 
         <h1 className='font-bold text-4xl'>Stake <br /> Amount:</h1>
         <p className="text-center mt-3 text-2xl">0</p>
     </div> 
