c6cd34ffdee19ad890e586bf730595057292326b
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-04-27 13:50:43 +0100

    update

--- src/components/displayPanel/DisplayPanel.jsx
+++ src/components/displayPanel/DisplayPanel.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React from "react";
+import StakeAmount from "./StakeAmount";
+import RewardRate from "./RewardRate";
+import EarnedReward from "./EarnedReward";
 
 const DisplayPanel = () => {
   return (
-    <div className='flex justify-around mt-7 	'>
-        <div className='text-4xl'>
-            <h1>Connected Account</h1>
-        </div>
-        <div>
-            <h2>Connected Account</h2>
-            <p>Unsupported</p>
-        </div>
-    </div>
-  )
-}
+    <div className=" mt-20	">
 
-export default DisplayPanel
+      <div className="flex justify-around">
+        <div className="text-4xl font-bold	">
+          <h1>Connected Account</h1>
+        </div>
+
+        <div>
+          <h2 className=" text-4xl font-bold">Connected Account</h2>
+          <p className="text-center mt-3 text-2xl">Unsupported</p>
+        </div>
+      </div>
+
+      <div className="flex justify-around px-40 mt-20">
+        <StakeAmount/>
+        <RewardRate />
+        <EarnedReward />
+
+
+      </div>
+    </div>
+  );
+};
+
+export default DisplayPanel;
---
+++ src/components/displayPanel/RewardRate.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RewardRate = () => {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>RewardRate</h1>
+        <p className="text-center mt-3 text-2xl">0</p>
+
+    </div>
+  )
+}
+
+export default RewardRate
