2ae56595fd37499f65e2e3668058f7572f27d882
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2024-05-25 13:53:50 +0100

    updat

--- contract/contracts/Staking.sol
+++ contract/contracts/Staking.sol
@@ -39,4 +39,8 @@ contract Staking is ReentrancyGuard {
 
     }
 
+    function earned(address account) public view returns(uint) {
+        return stakedBalance[account] * (rewardPerToken() -userRewardPerTokenPaid[account] + rewards[account])
+    }
+
 }
